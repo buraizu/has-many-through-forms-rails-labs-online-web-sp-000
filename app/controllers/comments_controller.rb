499fed3a74b4ce13e3bd997c8248a32ccbee1059
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-10 22:21:54 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    comment.user_id = params[:comment][:user_id]
     binding.pry
     redirect_to comment.post
   end
@@ -12,3 +13,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
+
+# {"utf8"=>"✓",
+#  "authenticity_token"=>
+#   "oYsgnYKRsufi0pW1fCTeYAV+lTPCkL4esGDW/XPUoPrT6YCCl0IEABHSaXR5w+Vt3l0QmfitlbaeLdmmaRZpew==",
+#  "comment"=>{"content"=>"comment4", "user_id"=>["1", ""]},
+#  "commit"=>"Create Comment",
+#  "controller"=>"comments",
+#  "action"=>"create"}
